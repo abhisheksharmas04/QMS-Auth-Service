71aa03c23514370330f0fd0313557b3b02caa90c
abhisheksharmas04 <abhisheksharmaas04@gmail.com>
2023-01-14 08:26:42 +0530

    project created

---
+++ src/main/java/com/ab/qms/admin/QmsAdminApplication.java
@@ -0,0 +1,14 @@
+package com.ab.qms.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmsAdminApplication.class, args);
+		System.out.println("Application running");
+	}
+
+}
---
+++ src/test/java/com/ab/qms/admin/QmsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.qms.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
